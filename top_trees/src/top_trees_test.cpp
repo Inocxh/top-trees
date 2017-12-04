750e35c35909e289f6f6d26708175bca1597eb7d
Jiří Setnička <setnicka@seznam.cz>
2017-12-05 00:43:15 +0100

    TopTrees: Updated testing usage

--- top_trees/src/top_trees_test.cpp
+++ top_trees/src/top_trees_test.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <memory>
+#include <string>
 
 #include "TopTree.hpp"
 
 struct MyClusterData: public TopTree::ClusterData {
 	int weight;
+	int total_weight;
+	std::string label;
 };
 
 class MyVertexData: public TopTree::VertexData {
@@ -23,7 +26,6 @@ public:
 	MyEdgeData(std::string label): label{label} {}
 
 	std::string label;
-	int weight;
 
 	virtual std::ostream& ToString(std::ostream& o) const {
 		//return o << "Edge " << label;
@@ -35,7 +37,15 @@ void TopTree::Join(std::shared_ptr<ClusterData> leftChild, std::shared_ptr<Clust
 	auto leftChild2 = std::dynamic_pointer_cast<MyClusterData>(leftChild);
 	auto rightChild2 = std::dynamic_pointer_cast<MyClusterData>(rightChild);
 	auto parent2 = std::dynamic_pointer_cast<MyClusterData>(parent);
-	parent2->weight = leftChild2->weight + rightChild2->weight;
+
+	if (isRake) parent2->weight = rightChild2->weight;
+	else parent2->weight = leftChild2->weight + rightChild2->weight;
+
+	parent2->total_weight = leftChild2->total_weight + rightChild2->total_weight;
+
+	// For debug:
+	// parent2->label = leftChild2->label + "," + rightChild2->label;
+	// std::cerr << "Joining " << leftChild2->total_weight << "(" << leftChild2->label << ") + " << rightChild2->total_weight << "(" << rightChild2->label << ")" << std::endl;
 }
 void TopTree::Split(std::shared_ptr<ClusterData> leftChild, std::shared_ptr<ClusterData> rightChild, std::shared_ptr<ClusterData> parent, bool isRake) {
 	// Nothing
@@ -44,7 +54,10 @@ void TopTree::Split(std::shared_ptr<ClusterData> leftChild, std::shared_ptr<Clus
 // Creating and destroying Base clusters:
 void TopTree::Create(std::shared_ptr<ClusterData> cluster, std::shared_ptr<EdgeData> edge) {
 	auto cluster2 = std::dynamic_pointer_cast<MyClusterData>(cluster);
+	auto edge2 = std::dynamic_pointer_cast<MyEdgeData>(edge);
 	cluster2->weight = 10;
+	cluster2->total_weight = 10;
+	cluster2->label = edge2->label;
 }
 void TopTree::Destroy(std::shared_ptr<ClusterData> cluster, std::shared_ptr<EdgeData> edge) {
 	// Nothing
@@ -78,69 +91,63 @@ int main(int argc, char const *argv[]) {
 	// Example from article:
 	auto z = baseTree->AddVertex(std::make_shared<MyVertexData>("z"));
 	auto p = baseTree->AddLeaf(z, std::make_shared<MyEdgeData>("z-p"), std::make_shared<MyVertexData>("p"));
-	auto w = baseTree->AddLeaf(p, NULL, std::make_shared<MyVertexData>("w"));
-	auto c = baseTree->AddLeaf(w, NULL, std::make_shared<MyVertexData>("c"));
-	auto b = baseTree->AddLeaf(c, NULL, std::make_shared<MyVertexData>("b"));
-	baseTree->AddLeaf(b, NULL, std::make_shared<MyVertexData>("a"));
+	auto w = baseTree->AddLeaf(p, std::make_shared<MyEdgeData>("p-w"), std::make_shared<MyVertexData>("w"));
+	auto c = baseTree->AddLeaf(w, std::make_shared<MyEdgeData>("w-c"), std::make_shared<MyVertexData>("c"));
+	auto b = baseTree->AddLeaf(c, std::make_shared<MyEdgeData>("c-b"), std::make_shared<MyVertexData>("b"));
+	auto a = baseTree->AddLeaf(b, std::make_shared<MyEdgeData>("b-a"), std::make_shared<MyVertexData>("a"));
 
-	baseTree->AddLeaf(b, NULL, std::make_shared<MyVertexData>("t"));
+	auto t = baseTree->AddLeaf(b, std::make_shared<MyEdgeData>("b-t"), std::make_shared<MyVertexData>("t"));
 
-	auto d = baseTree->AddLeaf(c, NULL, std::make_shared<MyVertexData>("d"));
-	baseTree->AddLeaf(d, NULL, std::make_shared<MyVertexData>("r"));
+	auto d = baseTree->AddLeaf(c, std::make_shared<MyEdgeData>("c-d"), std::make_shared<MyVertexData>("d"));
+	auto r = baseTree->AddLeaf(d, std::make_shared<MyEdgeData>("d-r"), std::make_shared<MyVertexData>("r"));
 
-	auto e = baseTree->AddLeaf(c, NULL, std::make_shared<MyVertexData>("e"));
-	baseTree->AddLeaf(e, NULL, std::make_shared<MyVertexData>("q"));
+	auto e = baseTree->AddLeaf(c, std::make_shared<MyEdgeData>("c-e"), std::make_shared<MyVertexData>("e"));
+	auto q = baseTree->AddLeaf(e, std::make_shared<MyEdgeData>("e-q"), std::make_shared<MyVertexData>("q"));
 
-	auto s = baseTree->AddLeaf(e, NULL, std::make_shared<MyVertexData>("s"));
+	auto s = baseTree->AddLeaf(e, std::make_shared<MyEdgeData>("e-s"), std::make_shared<MyVertexData>("s"));
 
-	baseTree->AddLeaf(c, NULL, std::make_shared<MyVertexData>("f"));
+	baseTree->AddLeaf(c, std::make_shared<MyEdgeData>("c-f"), std::make_shared<MyVertexData>("f"));
 
-	auto g = baseTree->AddLeaf(c, NULL, std::make_shared<MyVertexData>("g"));
-	baseTree->AddLeaf(g, NULL, std::make_shared<MyVertexData>("j"));
+	auto g = baseTree->AddLeaf(c, std::make_shared<MyEdgeData>("c-g"), std::make_shared<MyVertexData>("g"));
+	auto j = baseTree->AddLeaf(g, std::make_shared<MyEdgeData>("g-j"), std::make_shared<MyVertexData>("j"));
 
-	baseTree->AddLeaf(g, NULL, std::make_shared<MyVertexData>("h"));
+	auto h = baseTree->AddLeaf(g, std::make_shared<MyEdgeData>("g-h"), std::make_shared<MyVertexData>("h"));
 
-	auto n = baseTree->AddLeaf(c, NULL, std::make_shared<MyVertexData>("n"));
-	baseTree->AddLeaf(n, NULL, std::make_shared<MyVertexData>("k"));
+	auto n = baseTree->AddLeaf(c, std::make_shared<MyEdgeData>("c-n"), std::make_shared<MyVertexData>("n"));
+	baseTree->AddLeaf(n, std::make_shared<MyEdgeData>("n-k"), std::make_shared<MyVertexData>("k"));
 
-	auto v = baseTree->AddLeaf(n, NULL, std::make_shared<MyVertexData>("v"));
-	baseTree->AddLeaf(v, NULL, std::make_shared<MyVertexData>("u"));
+	auto v = baseTree->AddLeaf(n, std::make_shared<MyEdgeData>("n-v"), std::make_shared<MyVertexData>("v"));
+	baseTree->AddLeaf(v, std::make_shared<MyEdgeData>("v-u"), std::make_shared<MyVertexData>("u"));
 
-	baseTree->AddLeaf(v, NULL, std::make_shared<MyVertexData>("l"));
+	baseTree->AddLeaf(v, std::make_shared<MyEdgeData>("v-l"), std::make_shared<MyVertexData>("l"));
 
-	baseTree->AddLeaf(n, NULL, std::make_shared<MyVertexData>("i"));
+	baseTree->AddLeaf(n, std::make_shared<MyEdgeData>("n-i"), std::make_shared<MyVertexData>("i"));
 
-	baseTree->AddLeaf(w, NULL, std::make_shared<MyVertexData>("y"));
+	auto y = baseTree->AddLeaf(w, std::make_shared<MyEdgeData>("w-y"), std::make_shared<MyVertexData>("y"));
 
-	baseTree->AddLeaf(w, NULL, std::make_shared<MyVertexData>("o"));
+	auto o = baseTree->AddLeaf(w, std::make_shared<MyEdgeData>("w-o"), std::make_shared<MyVertexData>("o"));
 
-	baseTree->AddLeaf(p, NULL, std::make_shared<MyVertexData>("x"));
+	baseTree->AddLeaf(p, std::make_shared<MyEdgeData>("p-x"), std::make_shared<MyVertexData>("x"));
 
 	// baseTree->PrintRooted(z);
 
 	////////////////
 
-	auto T = new TopTree::TopTree(baseTree);
+	auto T = std::make_shared<TopTree::TopTree>(baseTree);
 
-	auto roots = T->GetTopTrees();
-	for (auto root : roots) {
-		// T->PrintRooted(root);
-		T->PrintGraphviz(root, "Initial Top Tree");
-	}
-
+	for (auto root : T->GetTopTrees()) T->PrintGraphviz(root, "Initial Top Tree");
 	auto node = T->Expose(s, e);
 	print_node(node);
-	auto nodes = T->Cut(p, z);
+
+	auto nodes = T->Cut(c, w);
 	print_node(std::get<0>(nodes));
 	print_node(std::get<1>(nodes));
-	//T->Expose(v, e);
-	roots = T->GetTopTrees();
-	for (auto root : roots) {
-		// T->PrintRooted(root);
-		T->PrintGraphviz(root, "Final Top Tree");
-	}
+	for (auto root : T->GetTopTrees()) T->PrintGraphviz(root, "After Cut");
 
-	delete T;
+	node = T->Link(p, j, std::make_shared<MyEdgeData>("p-j"));
+	print_node(node);
+	//T->Expose(v, e);
+	for (auto root : T->GetTopTrees()) T->PrintGraphviz(root, "After Link");
 
 	return 0;
 }
