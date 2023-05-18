8e8e135ab9d61556da91419776e7ebd580e810d2
Magnus Siegumfeldt <m.ks@outlook.dk>
2023-05-18 12:09:37 +0200

    hpc commit

---
+++ submit_connec_05_all.sh
@@ -0,0 +1,41 @@
+#!/bin/sh 
+
+### ---------------
+### General options 
+### ---------------
+
+
+### -- specify queue -- 
+#BSUB -q hpc
+
+### -- set the job Name -- 
+#BSUB -J TopTreeBench_connec_all_05
+
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 1 
+
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=16GB]"
+#BSUB -R "select[model == XeonE5_2660v3]"
+
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
+
+### -- set the email address -- 
+#BSUB -u s204472@student.dtu.dk
+
+### -- send notification at completion -- 
+#BSUB -N 
+
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o logs/Output_%J.out 
+#BSUB -e logs/Output_%J.err 
+
+# here follow the commands you want to execute with input.in as the input file
+
+python3 experiments/runner_double_edge_connectivity.py 0.5 1 > out/connec_all_05.out
+
+
+
+
